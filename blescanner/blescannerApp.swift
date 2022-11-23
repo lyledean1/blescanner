35fa5b3dc7e11f89bca1b25e1dc6679dd4a01ccc
Lyle Dean <dean@lyle.dev>
2022-11-23 17:35:52 +0000

    Initial Commit

---
+++ blescanner/blescannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  blescannerApp.swift
+//  blescanner
+//
+//  Created by Lyle Dean on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct blescannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
